9cc8a439a7f84eeedaf2bdc7e174243b75490267
pijarcandra22 <pijarcandra22@gmail.com>
2022-06-26 16:15:40 +0700

    First Data

---
+++ src/handler.js
@@ -0,0 +1,132 @@
+/* eslint-disable linebreak-style */
+/* eslint-disable eol-last */
+/* eslint-disable max-len */
+const { nanoid } = import('nanoid');
+const books = require('./books');
+
+const addBooksHandler = (request, h) => {
+  const {
+    name, year, author, summary, publisher, pageCount, readPage, reading,
+  } = request.payload;
+
+  const id = nanoid(16);
+  const insertedAt = new Date().toISOString();
+  const updatedAt = insertedAt;
+  const finished = false;
+
+  const newBook = {
+    id, name, year, author, summary, publisher, pageCount, readPage, finished, reading, insertedAt, updatedAt,
+  };
+
+  books.push(newBook);
+
+  const isSuccess = books.filter((note) => note.id === id).length > 0;
+
+  if (isSuccess) {
+    const response = h.response({
+      status: 'success',
+      message: 'Catatan berhasil ditambahkan',
+      data: {
+        noteId: id,
+      },
+    });
+    response.code(201);
+    return response;
+  }
+  const response = h.response({
+    status: 'fail',
+    message: 'Catatan gagal ditambahkan',
+  });
+  response.code(500);
+  return response;
+};
+
+const getAllBooksHandler = () => ({
+  status: 'success',
+  data: {
+    books,
+  },
+});
+
+const getBooksByIdHandler = (request, h) => {
+  const { bookid } = request.params;
+
+  const book = books.filter((n) => n.id === bookid)[0];
+  if (book !== undefined) {
+    return {
+      status: 'success',
+      data: {
+        book,
+      },
+    };
+  }
+  const response = h.response({
+    status: 'fail',
+    message: 'Catatan tidak ditemukan',
+  });
+  response.code(404);
+  return response;
+};
+
+const editBooksByIdHandler = (request, h) => {
+  const { bookid } = request.params;
+
+  const {
+    name, year, author, summary, publisher, pageCount, readPage, reading,
+  } = request.payload;
+  const updatedAt = new Date().toISOString();
+  const index = books.findIndex((note) => note.id === bookid);
+
+  if (index !== -1) {
+    books[index] = {
+      ...books[index],
+      name,
+      year,
+      author,
+      summary,
+      publisher,
+      pageCount,
+      readPage,
+      reading,
+      updatedAt,
+    };
+    const response = h.response({
+      status: 'success',
+      message: 'Catatan berhasil diperbarui',
+    });
+    response.code(200);
+    return response;
+  }
+  const response = h.response({
+    status: 'fail',
+    message: 'Gagal memperbarui catatan. Id tidak ditemukan',
+  });
+  response.code(404);
+  return response;
+};
+
+const deleteBooksByIdHandler = (request, h) => {
+  const { bookid } = request.params;
+
+  const index = books.findIndex((note) => note.id === bookid);
+  if (index !== -1) {
+    books.splice(index, 1);
+    const response = h.response({
+      status: 'success',
+      message: 'Catatan berhasil dihapus',
+    });
+    response.code(200);
+    return response;
+  }
+
+  const response = h.response({
+    status: 'fail',
+    message: 'Catatan gagal dihapus. Id tidak ditemukan',
+  });
+  response.code(404);
+  return response;
+};
+
+module.exports = {
+  addBooksHandler, getAllBooksHandler, getBooksByIdHandler, editBooksByIdHandler, deleteBooksByIdHandler,
+};
---
+++ src/routes.js
@@ -0,0 +1,40 @@
+/* eslint-disable linebreak-style */
+/* eslint-disable no-unused-vars */
+// eslint-disable-next-line import/newline-after-import
+const {
+  addBooksHandler,
+  getAllBooksHandler,
+  getBooksByIdHandler,
+  editBooksByIdHandler,
+  deleteBooksByIdHandler,
+} = require('./handler');
+
+const routes = [
+  {
+    method: 'POST',
+    path: '/books',
+    handler: addBooksHandler,
+  },
+  {
+    method: 'GET',
+    path: '/books',
+    handler: getAllBooksHandler,
+  },
+  {
+    method: 'GET',
+    path: '/books/{bookid}',
+    handler: getBooksByIdHandler,
+  },
+  {
+    method: 'PUT',
+    path: '/books/{bookid}',
+    handler: editBooksByIdHandler,
+  },
+  {
+    method: 'DELETE',
+    path: '/books/{bookid}',
+    handler: deleteBooksByIdHandler,
+  },
+];
+
+module.exports = routes;
